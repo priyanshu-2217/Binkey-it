bcb3595cc526ced3d42b275400876051c685228e
Priyanshu Sharma <162030924+priyanshu-2217@users.noreply.github.com>
2025-06-02 09:28:30 +0530

    Delete server directory

--- server/routes/user.route.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { registerUserController } from "../controllers/users.controllers.js";
-
-const userRouter = Router();
-
-userRouter.post("/register", registerUserController);
-console.log(userRouter);
-
-export default userRouter;
